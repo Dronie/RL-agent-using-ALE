ce3b6ce442a34db45f60360b172bfd3eecdaf6e1
Dronie <sroes002@gold.ac.uk>
2019-04-11 18:06:20 +0100

    made progress

--- RL_studies_code/A3C_breakout/gymTest/DQNBreakout.py
+++ RL_studies_code/A3C_breakout/gymTest/DQNBreakout.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gym
+import gym, random, collections
 
 from keras.models import Sequential, Model
 from keras.layers import *
@@ -8,30 +8,59 @@ from keras.optimizers import *
 # ---- PREPROCESSING ---- (Ecoffet, Adrian L: https://becominghuman.ai/lets-build-an-atari-ai-part-1-dqn-df57e8ff3b26)
 
 
-class preprocessor:
+class Preprocessor:
+    def __init__(self):
+        self.stack_size = 4
+        self.stacked_frames = collections.deque([np.zeros((105,80), dtype=np.int) for i in range(self.stack_size)], maxlen=4)
+        self.is_new_episode = True
+    
     def to_greyscale(self, img):
         return np.mean(img, axis=2).astype(np.uint8)
 
     def downsample(self, img):
         return img[::2, ::2]
+    
+    def normalize(self, img):
+        return img / 255.0
 
     def preprocess(self, img):
-        return self.to_greyscale(self.downsample(img))
+        return self.normalize(self.to_greyscale(self.downsample(img)))
     
-    def stack(self, img):
-        temp = []
+    def one_hot_encode(self, actions):
+        return np.array(np.identity(actions,dtype='int').tolist())
+    
+    def stack(self, state):
+        frame = self.preprocess(state)
         
+        if self.is_new_episode:
+            self.stacked_frames = collections.deque([np.zeros((105,80), dtype=np.int) for i in range(self.stack_size)], maxlen=4)
+            
+            self.stacked_frames.append(frame)
+            self.stacked_frames.append(frame)
+            self.stacked_frames.append(frame)
+            self.stacked_frames.append(frame)
+            
+            stacked_state = np.stack(self.stacked_frames, axis=2)
+        else:
+            self.stacked_frames.append(frame)
+            stacked_state = np.stack(self.stacked_frames, axis=2)
+        
+        return stacked_state, self.stacked_frames
     
 class DQN:
-    def __init__(self):
+    def __init__(self, env):
         self.model = self.create_model()
         self.process_model()
         self.gamma = 0.99
+        self.env = env
         
-    #input expected as an array of stacked states (shape of (x, 105, 80, 4))
+    #input expected as an array of stacked states (shape of (x, 4, 105, 80))
     def create_model(self):
-        state_input = Input(shape=(105, 80, 4)) # channels last (put channels first if you're having issues)
-        action_input = Input(shape=(4,))
+        ATARI_STATE_SPACE = (4, 105, 80)
+        ACTION_SPACE = 4,
+        
+        state_input = Input(shape=(ATARI_STATE_SPACE))
+        action_input = Input(shape=(ACTION_SPACE))
         
         conv_1 = convolutional.Conv2D(16, 8, 8, subsample=(4, 4), activation='relu')(state_input)
         conv_2 = convolutional.Conv2D(32, 4, 4, subsample=(2, 2), activation='relu')(conv_1)
@@ -60,7 +89,10 @@ class DQN:
 class Environment:
     def __init__(self, environment):
         self.environment = environment
-        self.env = gym.make(self.environment)                
+        self.env = gym.make(self.environment)
+
+    def return_action_space(self):
+        return self.env.action_space.n        
     
     def run(self, agent, render):
         state = self.env.reset()
@@ -81,6 +113,21 @@ class Memory:
     
     def __init__(self, max_capacity):
         self.max_capacity = max_capacity
+    
+    def add(self, sample):
+        self.samples.append(sample)
+        self.check_memory()
+    
+    def remove(self):
+        self.samples.pop(0)
+    
+    def sample(self, n):
+        n = min(n, len(self.samples))
+        return random.sample(self.samples, n)
+    
+    def check_memory(self):
+        while len(self.samples) > self.max_capacity:
+            self.remove()
 
 class Agent:
     def __init__(self):
@@ -105,7 +152,6 @@ def random_play():
             
 if __name__ == '__main__':
     prep = preprocessor()
-    dqn = DQN()
-    
-    
+    env = Environment('BreakoutDeterministic-v4')
+    dqn = DQN(env)
     

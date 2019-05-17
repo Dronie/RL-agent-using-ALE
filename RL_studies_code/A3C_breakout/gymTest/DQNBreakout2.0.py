f7bc3654804b0f43f8042855bd2ce9d33f6aedf5
Dronie <sroes002@gold.ac.uk>
2019-05-17 09:29:58 +0100

    finalised code

--- RL_studies_code/A3C_breakout/gymTest/DQNBreakout2.0.py
+++ RL_studies_code/A3C_breakout/gymTest/DQNBreakout2.0.py
@@ -2,9 +2,12 @@ import gym, collections, random, math, sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import *
-from keras.optimizers import *
+import tensorflow as tf
+#from keras.models import Sequential
+#from keras.layers import *
+#from keras.optimizers import *
+
+from tensorflow.keras.optimizers import *
 
 #np.set_printoptions(threshold=sys.maxsize)
 
@@ -15,13 +18,14 @@ epsilon = max_eps
 Lambda = 0.001
 steps = 0
 gamma = 0.99
+Q = 0
 
 # Create a breakout environment
 env = gym.make("BreakoutDeterministic-v4")
 
 # initialize a deque to prepare frames for stacking
 stacked_frames = collections.deque([np.zeros((105,80), dtype=np.int) for i in range(4)], maxlen=4)
-# initialize experience replay with max capacity of 1 million elements 
+# initialize experience replay with max capacity of 250,000 elements 
 experience_replay_memory = collections.deque(maxlen=250000)
 
 # function to convert state to greyscale 
@@ -36,8 +40,6 @@ def downsample(img):
 def preprocess(img):
     return to_greyscale(downsample(img))
 
-def normalize(img):
-    return img / 255.0
 # initializes frame stack deque to a full stack of the first state
 def init_img_stack(img):
     frame = preprocess(img)
@@ -50,35 +52,33 @@ def add_to_stack(img):
     stacked_frames.append(frame)
     return np.asarray(stacked_frames).reshape(105, 80, 4)
 
-# adds a stack to experience replay memory
-def add_stack_to_replay():
-    s = conv_stack_to_numpy_array()
-    experience_replay_memory.append(s)
+# define the Neural Network driving the Deep Q Network
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16, (8, 8), strides=4, activation='relu', input_shape=(105, 80, 4)),
+    tf.keras.layers.Conv2D(32, (4, 4), strides=2, activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(env.action_space.n)
+])
 
-# convert stack to numpy array (useful for manipulation by the network)
-def conv_stack_to_numpy_array():
-    return np.asarray(stacked_frames).reshape(105, 80, 4)
 
+# load weights of previously trained model
+model.load_weights("DQNBreakout.h5")
 
-# define the DQN, input is a 
-model = Sequential()
-
-model.add(Conv2D(16, (8, 8), strides=4, activation='relu', input_shape=(105, 80, 4)))
-model.add(Conv2D(32, (4, 4), strides=2, activation='relu'))
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dense(env.action_space.n))
-#model.load_weights("DQNBreakout.h5")
+# print a summary of the model, including the amount of traniable parameters
 model.summary()
 
+#define the optimizer of the model
 optimizer = RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+
+#compile the model with the previously defined optimizer and a Mean Squared Error loss function
 model.compile(optimizer, loss='mse')
 
-# function that returns a prediction on 'state', where 'state' is a set of preprocessed, stacked, and committed to experience replay, states 
+# function that returns a prediction on 'state', where 'state' is a set of preprocessed, stacked, and committed to experience replay states 
 def predict(state):
     return model.predict(state)
 
-# adds 'sample' to experience_replay_memory and adjusts epsilon based on the amount of steps taken so far
+# adds 'sample' to experience_replay_memory and adjusts epsilon based on the epsilon decay equation
 def observe(sample):
     global steps
     global epsilon
@@ -87,12 +87,14 @@ def observe(sample):
     steps += 1
     epsilon = min_eps + (max_eps - min_eps) * math.exp(-Lambda * steps)
 
-# epsilon-greedy (input (1, 105, 80, 4))
+# epsilon-greedy function, returns the action to take given 'state'
 def act(state):
     if random.random() < epsilon:
         return random.randint(0, env.action_space.n)
     else:
-        return np.argmax(predict(state))
+        global Q
+        Q = predict(state)
+        return np.argmax(Q)
 
 # returns a sample from experience_replay_memory of size 'batch_size'
 def sample(batch_size):
@@ -101,23 +103,32 @@ def sample(batch_size):
     else:
         return np.asarray(random.sample(experience_replay_memory, batch_size))
 
+# replay function, ultimately performs a single gradient descent step
 def replay():
+    # get 64 (s,a,r,s') samples
     batch = sample(64)
-
+    
+    # used as a placeholder for s' if the current state is terminal
     no_state = np.zeros(33600).reshape(105, 80, 4)
 
+    # set states to 's' from batch
     states = np.array([ observations[0] for observations in batch ])
+    
+    # set states to 's'' from batch
     states_ = np.array([ (no_state if done else observations[3]) for observations in batch ])
-
+    
+    # make Q(s,a) predictions on states and states_
     predictions = predict(states)
     predictions_ = predict(states_)
-
+    
+    # initialize input and label data sets
     x = np.zeros((64, 105, 80, 4))
-    y = np.zeros((64, 4))
+    y = np.zeros((64, env.action_space.n))
 
+    # for every frame, use predictions to make a target on which to perform gradient descent
     for i in range(len(batch)):
         o = batch[i]
-        s = o[0]; a = o[1]; r = o[2]; s_ = o[3]
+        s = o[0]; a = o[1]; r = o[2]
 
         target = predictions[i]
         if done:
@@ -128,38 +139,70 @@ def replay():
         x[i] = s
         y[i] = target
     
-    model.fit(x, y, batch_size=64, nb_epoch=1, verbose=0)
+    model.fit(x, y, batch_size=64, epochs=1, verbose=0)
 
-rewards = []
 episodes = 10000
+
+# begin training
 for i in range(episodes):
+    # At the start of each episode:
+    
+    # save model
+    model.save("DQNBreakout.h5")
+    
+    # print current episode
+    print("Current episode:", i + 1, "/", episodes)
+    
+    # make sure the 'done' variable is set to false
+    global done
     done = False
     
-    # Reset it, returns the starting frame
+    # reset the environment
     frame = env.reset()
-    # Render
-    #env.render()
+    
+    # render the environment
+    env.render()
+    
+    # Initialize the frame stack with the first frame of the game
     init_img_stack(frame)
+    
+    # set s to the current state of the frame stack
     s = add_to_stack(frame)
+    
+    # reset total reward to 0
     R = 0
-    #add_stack_to_replay()
-    while True:    
+    
+    while True:
+        # For each state:
+        
+        # set the current action (returned by act()) to a
         a = act(np.asarray([s]))
-
+        
+        # perform action a storing the next state, reward and whether the state is terminal (info is essentially ignored)
         s_, r, done, info = env.step(a-1)
-        #env.render()
-        print("Current episode:", i, "/", episodes)
+        
+        # render the new frame
+        env.render()
+        
+        # ass s_ to the frame stack and store it as s_
         s_ = add_to_stack(s_)
 
+        # add the most recent (s, a, r, s') tuple to memory and increment one step of epsilon greedy decay
         observe( (s, a, r, s_) )
+        
+        # use a sample from memory to predict targets and perform a single gradient descent step
         replay()
-
+        
+        # set the current state as the next state
         s = s_
+        
+        # Increment the total reward
         R += r
-        print("Current total reward:", R)
+        
+        # if a terminal state is reached, start a new episode
         if done:
             break
-    rewards.append(R)
-plt.plot(range(episodes), rewards)
+    
+    print("Previous Reward:", R)
 
-model.save("DQNBreakout.h5")
+#model.save("DQNPong.h5")
